34240d6990bda2b06966ba66ef7d3b9811680f59
yousuzume <den.apolinari@mail.ru>
2025-06-06 13:18:27 +0300

    Add files via upload

---
+++ Алгоритмы.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество строк матрицы: ");
+        int rows = scanner.nextInt();
+        System.out.print("Введите количество столбцов матрицы: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Введите элементы матрицы построчно:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        int[][] transposed = new int[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                transposed[i][j] = matrix[j][i];
+            }
+        }
+
+        System.out.println("Транспонированная матрица:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(transposed[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close(); 
+    }
+}
