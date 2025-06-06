2776cea6a2ccbe262f57cc083f97aaedae5f3323
yousuzume <den.apolinari@mail.ru>
2025-06-06 13:42:23 +0300

    Add files via upload

--- Алгоритмы.java
+++ Алгоритмы.java
@@ -1,39 +1,20 @@
-// 1 вариант 1 эссе
+// 1 вариант 2 эссе 
+
 import java.util.Scanner;
 
-public class Main {
+public class Greeting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите количество строк матрицы: ");
-        int rows = scanner.nextInt();
-        System.out.print("Введите количество столбцов матрицы: ");
-        int cols = scanner.nextInt();
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.nextLine();
 
-        int[][] matrix = new int[rows][cols];
-        System.out.println("Введите элементы матрицы построчно:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                matrix[i][j] = scanner.nextInt();
-            }
-        }
+        String formattedName = name.substring(0, 1).toUpperCase()
+                + name.substring(1).toLowerCase();
 
-        int[][] transposed = new int[cols][rows];
-        for (int i = 0; i < cols; i++) {
-            for (int j = 0; j < rows; j++) {
-                transposed[i][j] = matrix[j][i];
-            }
-        }
+        System.out.println("Привет, " + formattedName + "!");
 
-        System.out.println("Транспонированная матрица:");
-        for (int i = 0; i < cols; i++) {
-            for (int j = 0; j < rows; j++) {
-                System.out.print(transposed[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        scanner.close(); 
+        scanner.close();
     }
 }
